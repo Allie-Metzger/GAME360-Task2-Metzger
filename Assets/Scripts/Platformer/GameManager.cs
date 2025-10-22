b4daf478a27b3e47807c04d3bb03433c177f1244
Allie-Metzger <cmetz005@odu.edu>
2025-10-22 10:42:26 -0400

    [DEV] Removed R to Restart

--- Assets/Scripts/Platformer/GameManager.cs
+++ Assets/Scripts/Platformer/GameManager.cs
@@ -58,10 +58,10 @@ public class GameManager : MonoBehaviour
         }
 
         // Restart input
-        if (!isGameActive && Input.GetKeyDown(KeyCode.R))
+        /*if (!isGameActive && Input.GetKeyDown(KeyCode.R))
         {
             RestartGame();
-        }
+        }*/
     }
 
     void InitializeGame()
