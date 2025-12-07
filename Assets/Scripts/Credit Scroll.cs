900743fe6925050d42573602902cbe5fe9337f0e
Allie-Metzger <cmetz005@odu.edu>
2025-12-07 18:51:40 -0500

    [DEV] Streamlined Credits
    Automatically boots player to main menu after credits are done.
    
    Implemented according to https://docs.unity3d.com/ScriptReference/MonoBehaviour.Invoke.html

--- Assets/Scripts/Credit Scroll.cs
+++ Assets/Scripts/Credit Scroll.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditScroll : MonoBehaviour
 {
+    //private int timeinCredits = 0;
+
     public float speed = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Invoke(nameof(ReturnToMainMenu), 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+        //once time on screen = 60 seconds, go to main menu
+        //SceneManager.LoadScene("Start Menu");
+        
     }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }    
 }
